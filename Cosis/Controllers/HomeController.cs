92954c65fd296f1c8ff58676c2949341e22602b4
admin <xuandieu12300@gmail.com>
2022-12-08 17:32:29 +0700

    update _master2 + A_C 2.1 and 2.3

--- Cosis/Controllers/HomeController.cs
+++ Cosis/Controllers/HomeController.cs
@@ -42,5 +42,29 @@ namespace Cosis.Controllers
             var a = context.ThongTinDoanhNghiep.FromSqlRaw("select*from ThongTinDoanhNghiep where MaSoThue = {0} and MaSoThue2 = {1}",mst1,mst2).FirstOrDefault();
             return PartialView("_Master",a);
         }
+        // xd
+        [HttpPost("/loadMaster2")]
+        public IActionResult loadInfor(string ms)
+        {
+            FormCosisContext context = new FormCosisContext();
+            string mst1 = ms.Substring(0, 10);
+            string mst2 = ms.Substring(10);
+            var a = context.ThongTinDoanhNghiep.FromSqlRaw("select*from ThongTinDoanhNghiep where MaSoThue = {0} and MaSoThue2 = {1}", mst1, mst2).FirstOrDefault();
+            var a1 = context.ThongTinCaThe.FromSqlRaw("select*from ThongTinCaThe where MaSoThue = {0} and MaSoThue2 = {1}", mst1, mst2).FirstOrDefault();
+            var b = context.Master.FromSqlRaw("select*from Master where MaSoThue = {0} and MaSoThue2 = {1}", mst1, mst2).FirstOrDefault();
+            Console.WriteLine(a1);
+            if (a1 == null)
+            {
+                a1 = new ThongTinCaThe();
+                b.MaSoThueNavigation = a;
+                b.MaCoSoNavigation =a1;
+            }
+            else
+            {
+                b.MaCoSoNavigation = a1;
+                b.MaSoThueNavigation = a;
+            }
+            return PartialView("_Master2_1", b);
+        }
     }
 }
